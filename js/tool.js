07e3b998f0b4389408d4d55c0400e691c3fa6371
borkenegg <764141889@qq.com>
2017-08-18 15:04:02 +0800

    mmnv0.0.5

---
+++ js/tool.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2017/8/18.
+ */
+function getId(id) {
+    return document.getElementById(id)?document.getElementById(id):null;
+}
+
+function moving(speed,terminal,isleft){
+    var list = getId("index_img_list");
+    var left_val = parseInt(list.style.left);
+    var btn_idx;
+    var button = getId("index_imgBtn").getElementsByTagName("span");
+
+    if(left_val===terminal){
+        left_val=isleft;
+    }else{
+    left_val += speed;
+    }
+    list.style.left=left_val+"px";
+    btn_idx=Math.ceil(Math.abs(left_val/(-1906)));
+    for(var k=0;k<button.length;k++){
+        button[k].setAttribute("class","")
+    }
+    button[btn_idx].setAttribute("class","on");
+
+}
+function TXTmoving(speed,terminal){
+    var TXTlist=getId("index_messageUl");
+    var top_val = parseInt(TXTlist.style.top);
+    if(top_val===terminal){
+        top_val=0;
+    }else{
+        top_val += speed;
+    }
+    TXTlist.style.top=top_val+"px";
+
+}
