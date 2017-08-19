178caf07b3637a45d964c158285d0d123f22895b
Cynicism-conan <1334081722@qq.com>
2017-08-20 00:25:02 +0800

    version zyn 0.2

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ window.onload=function() {
     var index_example_next = document.getElementById("index_example_next");
     var index_example_list = document.getElementById("index_example_pic");
     var index_example_button = document.getElementById("index_example_buttons").getElementsByTagName("span");
-   //绑定事件
+    //绑定事件
     index_example_prev.addEventListener("click", function () {
         moving(1200, 0, "-2400px");
     });
@@ -24,10 +24,10 @@ window.onload=function() {
             }
         })(i);
     }
-   //编写函数
-    function moving(speed,terminal,flag) {
+    //编写函数
+    function moving(speed, terminal, flag) {
         var index_left_value = parseInt(index_example_list.style.left);
-        if (index_left_value ===terminal) {
+        if (index_left_value === terminal) {
             index_example_list.style.left = flag;
             index_left_value = parseInt(index_example_list.style.left);
         } else {
@@ -39,7 +39,8 @@ window.onload=function() {
         }
         index_example_button[index_left_value / -1200].setAttribute("class", "on");
     }
-     //控制下方的圆圈切换
+
+    //控制下方的圆圈切换
     function circle(that, n) {
         for (var i = 0; i < index_example_button.length; i++) {
             index_example_button[i].setAttribute("class", "");
@@ -47,10 +48,11 @@ window.onload=function() {
         that.setAttribute("class", "on");
         index_example_list.style.left = n * (-1200) + "px";
     }
+
 //定时器
     timer = setInterval(function () {
         moving(-1200, -2400, "0px");
-    }, 2000);
+    }, 1700);
 
     container.onmouseover = function () {
         clearInterval(timer);
@@ -62,11 +64,27 @@ window.onload=function() {
         clearInterval(timer1);
         timer1 = setInterval(function () {
             moving(-1200, -2400, "0px");
-        }, 2000);
+        }, 1700);
+    }
+//        风控无人机部分切换
+        var index_function_focus = document.getElementsByClassName("index_icon");
+        var index_function_detail = document.getElementsByClassName("index_bd");
+        for (var k = 0; k < index_function_focus.length; k++) {
+            index_function_focus[k].index = k;
+            index_function_focus[k].onmouseover = function () {
+                for (var j = 0; j < index_function_detail.length; j++) {
+                    index_function_detail[j].style.display = "none";
+                    index_function_focus[j].style = "black";
+                }
+                this.style.color = "#1c9cd1";
+                index_function_detail[this.index].style.display = "block";
+            }
+        }
+
+
         //人物切换
         var index_focus_obj = document.getElementsByClassName("index_person_dec");
         var index_details_obj = document.getElementsByClassName("index_person_detail");
-
         for (var m = 0; m < index_focus_obj.length; m++) {
             index_focus_obj[m].index = m;
             index_focus_obj[m].onmouseover = function () {
@@ -93,6 +111,5 @@ window.onload=function() {
                 index_news_detail[this.index].style.display = "block";
             }
         }
+}
 
-    }
-};
