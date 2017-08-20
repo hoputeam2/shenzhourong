12f26e0ebccde346e2826942c9270e269a393b61
housheng <764141889@qq.com>
2017-08-20 21:42:56 +0800

    mmnv0.1.0

--- js/production.js
+++ js/production.js
@@ -28,6 +28,29 @@ function navscroll(){
     }
 
 }
+        function moving(speed,terminal,isleft){
+            var list = getId("pro_carouselist");
+            var left_val = parseInt(list.style.left);
+            var btn_idx;
+            var button = document.getElementsByTagName("p");
+
+            if(left_val===terminal){
+                left_val=isleft;
+            }else{
+                left_val += speed;
+            }
+            list.style.left=left_val+"px";
+            btn_idx=Math.ceil(Math.abs(left_val/(-1261)));
+
+
+            for(var k=0;k<button.length;k++){
+                button[k].setAttribute("class","")
+            }
+            button[btn_idx].setAttribute("class","on");
+
+        }
+
+
 
 
 
