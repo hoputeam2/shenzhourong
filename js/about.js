b823cc6ca09e71a69275f6bb33c261c9a28d3878
cerfvolant <2623656148@qq.com>
2017-08-20 19:48:52 +0800

    version 0.0.4

---
+++ js/about.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lenovo on 2017/8/20.
+ */
+document.onscroll2=function(){
+    var tit2=document.getElementsByIdName("about-containerNav")[0];
+    var tittop2=tit2.offsetTop;
+    var btop2=document.body.scrollTop||document.documentElement.scrollTop;
+    if(btop2>tittop2){
+        tit2.setAttribute()
+    }
+}
