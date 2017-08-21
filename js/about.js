fd1cece96d3e7cf32d69a490cef8faf0733e2b3e
qian <www.2623656148@qq.com>
2017-08-21 18:02:50 +0800

    version 0.0.5

--- js/about.js
+++ js/about.js
@@ -1,11 +1,14 @@
 /**
  * Created by lenovo on 2017/8/20.
  */
-document.onscroll2=function(){
+document.onscroll=function(){
     var tit2=document.getElementsByIdName("about-containerNav")[0];
-    var tittop2=tit2.offsetTop;
+    //var tittop2=tit2.offsetTop;
     var btop2=document.body.scrollTop||document.documentElement.scrollTop;
-    if(btop2>tittop2){
-        tit2.setAttribute()
+    if(btop2>300){
+        tit2.setAttribute("style","background-color:white;color:black;border:1px solid black;");
+    }
+    else{
+        tit2.setAttribute("style","");
     }
 }
