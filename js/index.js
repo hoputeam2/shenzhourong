00bb33895bdd70a19f95c03d84a29aa0eb5124b9
housheng <764141889@qq.com>
2017-08-22 20:06:37 +0800

    zyn

--- js/index.js
+++ js/index.js
@@ -2,6 +2,108 @@
  * Created by Administrator on 2017/8/18.
  */
 window.onload=function() {
+    function getId(id) {
+        return document.getElementById(id)?document.getElementById(id):null;
+    }
+    /*大图滚动*/
+    function picmoving(speed,terminal,isleft){
+        var list = getId("index_img_list");
+        var left_val = parseInt(list.style.left);
+        var btn_idx;
+        var button = getId("index_imgBtn").getElementsByTagName("span");
+
+        if(left_val===terminal){
+            left_val=isleft;
+        }else{
+            left_val += speed;
+        }
+        list.style.left=left_val+"px";
+        btn_idx=Math.ceil(Math.abs(left_val/(-1906)));
+        for(var k=0;k<button.length;k++){
+            button[k].setAttribute("class","")
+        }
+        button[btn_idx].setAttribute("class","on");
+
+    }
+    /*最新消息滚动*/
+    function TXTmoving(speed,terminal){
+        var TXTlist=getId("index_messageUl");
+        var top_val = parseInt(TXTlist.style.top);
+        if(top_val===terminal){
+            top_val=0;
+        }else{
+            top_val += speed;
+        }
+        TXTlist.style.top=top_val+"px";
+
+    }
+    /*大图滚动*/
+    var content = getId("index_carouse");
+    var list = getId("index_img_list");
+    var prev = getId("navPrevPic");
+    var next = getId("navNextPic");
+    var button = getId("index_imgBtn").getElementsByTagName("span");
+    content.timer = null;
+
+    next.addEventListener("click", function () {
+        picmoving(-1905, -3810, 0);
+    });
+    prev.addEventListener("click", function () {
+        picmoving(1905, 0, -3810);
+    });
+
+    for (var i = 0; i < button.length; i++) {
+        (function (n) {
+            button[i].addEventListener("mouseover", function () {
+                list.style.left = n * (-1905) + "px";
+                for (var j = 0; j < button.length; j++) {
+                    button[j].setAttribute("class", "")
+                }
+                button[n].setAttribute("class", "on")
+            })
+
+        })(i)
+
+    }
+
+    content.timer = setInterval(function () {
+        picmoving(-1905, -3810, 0);
+    }, 3000);
+    content.onmouseover = function () {
+        clearInterval(content.timer);
+    };
+    content.onmouseout = function () {
+        clearInterval(content.timer);
+        content.timer = setInterval(function () {
+            picmoving(-1905, -3810, 0);
+        }, 3000);
+    };
+
+    /*最新消息的文字滚动*/
+    var TXTlist = getId("index_messageUl");
+    TXTlist.timer=null;
+    TXTlist.timer = setInterval(function () {
+        TXTmoving(-35, -70);
+    }, 4000);
+    TXTlist.onmouseover = function () {
+        clearInterval(TXTlist.timer);
+    };
+    TXTlist.onmouseout = function () {
+        clearInterval(TXTlist.timer);
+        TXTlist.timer = setInterval(function () {
+            TXTmoving(-35,-70);
+        }, 4000);
+    };
+
+
+
+
+
+
+
+
+
+
     //轮播部分
     var timer = null;
     var timer1 = null;
